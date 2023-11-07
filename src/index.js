1a7683ca69b4fdb60ed502db1bef9dce759d585d
Ibnu Choldun <ibnucholdun023@gmail.com>
2023-11-07 14:15:51 +0700

    feat: add api documentation with swagger

--- src/index.js
+++ src/index.js
@@ -2,17 +2,42 @@ const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
 const router = require("./routes/route");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Challenge 5",
+      version: "1.0.0",
+      description: "Challenge 5",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/api/v1",
+      },
+      {
+        url: "http://localhost:8080/api/v1",
+      },
+    ],
+  },
+  apis: ["./src/routes/*route.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", router);
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.get("/", (req, res) => {
-  res.send("Challenge 4");
+  res.send("Challenge 5");
 });
 
 app.listen(PORT, () => {
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -9,10 +9,140 @@ const {
 } = require("../controllers/user.controller");
 const { CheckPostReq } = require("../middleware/middleware");
 
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     tags:
+ *      - "User"
+ *     summary: Create User
+ *     description: Create new user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *              email:
+ *                type: string
+ *              password:
+ *                type: string
+ *              identity_type:
+ *                type: string
+ *              identity_number:
+ *                type: string
+ *              address:
+ *                type: string
+ *     responses:
+ *      201:
+ *        description: User created successfully
+ *      500:
+ *        description: Internal server error
+ */
+
 router.post("/", CheckPostReq, createUser);
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     tags:
+ *      - "User"
+ *     summary: Get All User
+ *     description: Return list of all user
+ *     responses:
+ *      200:
+ *        description: Users retrieved successfully
+ *      404:
+ *        description: User not found
+ *      500:
+ *        description: Internal server error
+ */
 router.get("/", getAllUser);
+
+/**
+ * @swagger
+ * /users/{userId}:
+ *   get:
+ *     tags:
+ *      - "User"
+ *     summary: Get one User
+ *     description: Return one user
+ *     parameters:
+ *      - in: path
+ *        name: userId
+ *     responses:
+ *      200:
+ *        description: Users retrieved successfully
+ *      404:
+ *        description: User not found
+ *      500:
+ *        description: Internal server error
+ */
 router.get("/:userId", getUserById);
+
+/**
+ * @swagger
+ * /users/{userId}:
+ *   put:
+ *     tags:
+ *      - "User"
+ *     summary: Update User
+ *     description: Update user
+ *     parameters:
+ *      - in: path
+ *        name: userId
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *              email:
+ *                type: string
+ *              password:
+ *                type: string
+ *              identity_type:
+ *                type: string
+ *              identity_number:
+ *                type: string
+ *              address:
+ *                type: string
+ *     responses:
+ *      200:
+ *        description: User updated successfully
+ *      404:
+ *        description: User not found
+ *      500:
+ *        description: Internal server error
+ */
 router.put("/:userId", updateUser);
+
+/**
+ * @swagger
+ * /users/{userId}:
+ *   delete:
+ *     tags:
+ *      - "User"
+ *     summary: Delete One User
+ *     description: Delete One user
+ *     parameters:
+ *      - in: path
+ *        name: userId
+ *     responses:
+ *      200:
+ *        description: User deleted successfully
+ *      404:
+ *        description: User not found
+ *      500:
+ *        description: Internal server error
+ */
 router.delete("/:userId", deleteUser);
 
 module.exports = router;
